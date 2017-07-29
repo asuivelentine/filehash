51bd6fcf16de5f9096aa965a33e4eb6319532e54
Kai Sickeler <k.sickeler@gmail.com>
2017-07-29 16:34:42 +0200

    Initial version

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#[deny(non_camel_case_types,
+       non_snake_case,
+       unused_import_braces,
+       trivial_numeric_casts,
+       unstable_features,
+       unused_allocation,
+       unused_imports,
+       unused_must_use,
+       unused_mut,
+       unused_qualifications,
+       while_true,
+       unsafe_code)]
+
+extern crate pwhash;
+extern crate twox-hash;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
