430582c4ee538656113fea834da43518b5ce9aa4
Kai Sickeler <k.sickeler@gmail.com>
2017-07-29 20:38:46 +0200

    Impl read_to_u8

--- src/filehash.rs
+++ src/filehash.rs
@@ -1,4 +1,6 @@
 use std::ffi::OsString;
+use std::fs::OpenOptions;
+use std::io::Read;
 
 pub struct Filehash {
     file: OsString,
@@ -29,8 +31,25 @@ impl Filehash {
         self
     }
 
-    pub fn hash(self) {
-        println!("Todo");
+    pub fn hash(self) -> Result<String, ()> {
+        try!(self.read_to_u8());
+
+        Ok(String::from("hash"))
+    }
+
+    fn read_to_u8(&self) -> Result<Vec<u8>, ()> {
+        let mut content = Vec::<u8>::new();
+
+        let mut f = try!(OpenOptions::new()
+            .read(true)
+            .create(false)
+            .open(&self.file)
+            .map_err(|_| ()));
+
+        try!(f.read_to_end(&mut content)
+            .map_err(|_| ()));
+
+        Ok(content)
     }
 }
 
@@ -38,12 +57,15 @@ impl Filehash {
 #[cfg(test)]
 mod tests {
     use super::{Filehash, Hash};
+    use std::ffi::OsString;
 
     #[test]
     fn new() {
         let file = OsString::from("/bin/bash");
-
         let fh = Filehash::new(file);
+
+        assert_eq!(fh.file, OsString::from("/bin/bash"));
+        assert_eq!(fh.hash, None);
     }
 }
 
